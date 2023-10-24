b77b698f7e203c882218bc9de196db73c16f2663
Ornela <ornela.mullaymeri30@outlook.com>
2023-10-25 00:48:10 +0200

    update

---
+++ src/main/java/eliteelectronics/EliteElectronicsApplication.java
@@ -0,0 +1,13 @@
+package eliteelectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteElectronicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EliteElectronicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eliteelectronics/EliteElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package eliteelectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteElectronicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

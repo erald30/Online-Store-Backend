79002ed53d6a50a1de53d8f8ccf0df0bd3db11d5
erald <eraldskura@gmail.com>
2023-10-25 12:26:08 +0200

    Creating OrderItem entity

---
+++ src/main/java/entities/OrderItem.java
@@ -0,0 +1,31 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "en_order_item")
+public class OrderItem extends BasEntity{
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_title")
+    private String productTitle;
+
+    @Column(name = "product_price")
+    private double productPrice;
+
+    @Column(name = "product_sale")
+    private boolean productSale;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "order_id")
+    private long orderId;
+}
